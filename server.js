ace652b32da836e7a573eeacd1140aec33d1f9f7
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-08-28 18:59:35 -1000

    finished

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const home = require('./router/home');
-const about = require('./router/about');
-const contact = require('./router/contact');
+const home = require("./routes/home/index");
+const about = require("./routes/about/index");
+const contact = require("./routes/contact/index");
 
-app.use(express.static('public'));
-app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'main'}));
-app.set('view engine', '.hbs');
+app.use(express.static("public"));
+app.engine(".hbs", exphbs({ extname: ".hbs", defaultLayout: "main" }));
+app.set("view engine", ".hbs");
 
-app.get('/', (req, res) => {
-  res.send('smoke test');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.use(home);
-app.use(about);
-app.use(contact);
+app.use("/home", home);
+app.use("/about", about);
+app.use("/contact", contact);
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
